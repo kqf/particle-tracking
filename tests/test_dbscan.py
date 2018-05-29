44fcd115bf59b9ca97ae8665ac21dece834429f5
Sasha <q.dev.null@gmail.com>
2018-05-29 10:42:53 +0200

    Clean tests

--- tests/test_dbscan.py
+++ tests/test_dbscan.py
@@ -1,4 +1,3 @@
-import unittest
 import numpy as np
 
 from trackml.dataset import load_dataset
@@ -11,34 +10,30 @@ TRAIN_DATASET = "data/train_100_events"
 TEST_DATASET = "data/test/"
 
 
-class TestDBSCAN(unittest.TestCase):
+def test_training_set():
+    dataset_scores = []
 
-    def test_training_set(self):
-        dataset_submissions = []
-        dataset_scores = []
+    end_id = 3
+    model = dbscan_model(eps=0.01)
+    for event_id, hits, cells, particles, truth in load_dataset(
+            TRAIN_DATASET):
 
-        end_id = 3
-        model = dbscan_model(eps=0.01)
-        for event_id, hits, cells, particles, truth in load_dataset(
-                TRAIN_DATASET):
+        if event_id >= end_id:
+            break
 
-            if event_id >= end_id:
-                break
+        # Track pattern recognition
+        labels = model.fit_predict(hits)
+        print(labels, len(labels))
+        print("HERE")
 
-            # Track pattern recognition
-            labels = model.fit_predict(hits)
-            print(labels, len(labels))
-            print("HERE")
+        # Prepare submission for an event
+        one_submission = create_one_event_submission(
+            event_id, hits, labels)
 
-            # Prepare submission for an event
-            one_submission = create_one_event_submission(
-                event_id, hits, labels)
-            dataset_submissions.append(one_submission)
+        # Score for the event
+        score = score_event(truth, one_submission)
+        dataset_scores.append(score)
 
-            # Score for the event
-            score = score_event(truth, one_submission)
-            dataset_scores.append(score)
+        print("Score for event %d: %.3f" % (event_id, score))
 
-            print("Score for event %d: %.3f" % (event_id, score))
-
-        print('Mean score: %.3f' % (np.mean(dataset_scores)))
+    print('Mean score: %.3f' % (np.mean(dataset_scores)))
--- tests/test_loads_data.py
+++ tests/test_loads_data.py
@@ -1,21 +1,19 @@
-import unittest
 from trackml.dataset import load_event
 
 
-class TestInputData(unittest.TestCase):
+# NB: Use this sort of tests to check the dataset
+def test_data():
+    hits, cells, particles, truth = load_event(
+        "data/train_100_events/event000001064"
+    )
+    print('Hits')
+    print(hits.head())
 
-    def test_data(self):
-        hits, cells, particles, truth = load_event(
-            "data/train_100_events/event000001064"
-        )
-        print('Hits')
-        print(hits.head())
+    print('Cells')
+    print(cells.head())
 
-        print('Cells')
-        print(cells.head())
+    print('Particles')
+    print(particles.head())
 
-        print('Particles')
-        print(particles.head())
-
-        print('Truth')
-        print(truth.head())
+    print('Truth')
+    print(truth.head())
