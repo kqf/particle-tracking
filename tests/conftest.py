094f2affb2357fd68535a7c25a48fc5b8af59a72
Sasha <q.dev.null@gmail.com>
2018-05-30 09:21:43 +0200

    Use fixtures for dbscan tests

---
+++ tests/conftest.py
@@ -0,0 +1,10 @@
+import pytest
+import itertools
+from trackml.dataset import load_dataset
+
+TRAIN_DATASET = 'data/train_100_events'
+
+
+@pytest.fixture()
+def training_set(n_events=2):
+    return itertools.islice(load_dataset(TRAIN_DATASET), n_events)
--- tests/test_dbscan.py
+++ tests/test_dbscan.py
@@ -1,39 +1,25 @@
 import numpy as np
 
-from trackml.dataset import load_dataset
 from trackml.score import score_event
 from model.dbscan import dbscan_model
 from model.utils import create_one_event_submission
 
 
-TRAIN_DATASET = "data/train_100_events"
-TEST_DATASET = "data/test/"
-
-
-def test_training_set():
+def test_training_set(training_set):
     dataset_scores = []
 
-    end_id = 3
     model = dbscan_model(eps=0.01)
-    for event_id, hits, cells, particles, truth in load_dataset(
-            TRAIN_DATASET):
-
-        if event_id >= end_id:
-            break
-
+    print()
+    for event_id, hits, cells, particles, truth in training_set:
         # Track pattern recognition
         labels = model.fit_predict(hits)
-        print(labels, len(labels))
-        print("HERE")
 
         # Prepare submission for an event
-        one_submission = create_one_event_submission(
-            event_id, hits, labels)
+        submission = create_one_event_submission(event_id, hits, labels)
 
         # Score for the event
-        score = score_event(truth, one_submission)
+        score = score_event(truth, submission)
         dataset_scores.append(score)
-
         print("Score for event %d: %.3f" % (event_id, score))
 
     print('Mean score: %.3f' % (np.mean(dataset_scores)))
